0bbe68d43fdcf544963d1b4d1708354e8f546565
“TobiasTheDanish“ <t.hc199811@gmail.com>
2025-07-31 11:56:50 +0200

    feat(Gs1Object): add helper to create inline Gs1Object

--- BarcodeScannerModule/src/main/java/dk/skancode/barcodescannermodule/gs1/Gs1Object.kt
+++ BarcodeScannerModule/src/main/java/dk/skancode/barcodescannermodule/gs1/Gs1Object.kt
@@ -5,5 +5,8 @@ typealias Gs1Object = MutableMap<Gs1AI, String>
 fun emptyGs1Object(): Gs1Object {
     return HashMap()
 }
+fun gs1ObjectOf(vararg pairs: Pair<Gs1AI, String>): Gs1Object {
+    return hashMapOf(*pairs)
+}
 
 data class Gs1AI(val ai: String)
