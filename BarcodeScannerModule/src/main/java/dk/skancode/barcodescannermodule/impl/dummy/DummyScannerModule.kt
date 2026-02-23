33e0f9332734329c28812182365581fe59787ff7
“TobiasTheDanish“ <t.hc199811@gmail.com>
2026-02-23 10:28:55 +0100

    chore: make DummyScannerModule a stub, to avoid unnecessary crashes

--- BarcodeScannerModule/src/main/java/dk/skancode/barcodescannermodule/impl/dummy/DummyScannerModule.kt
+++ BarcodeScannerModule/src/main/java/dk/skancode/barcodescannermodule/impl/dummy/DummyScannerModule.kt
@@ -9,84 +9,38 @@ import dk.skancode.barcodescannermodule.event.TypedEventHandler
 import dk.skancode.barcodescannermodule.gs1.Gs1Config
 
 class DummyScannerModule: IScannerModule {
-    override fun init() {
-    }
-
+    override fun init() { }
     override fun getScannerState(): String {
-        throw RuntimeException("Scanner is not available")
+        return "on"
     }
-
-    override fun setScannerState(enabler: Enabler) {
-        throw RuntimeException("Scanner is not available")
-    }
-
+    override fun setScannerState(enabler: Enabler) { }
     @Deprecated("Use registerTypedEventHandler and unregisterTypedEventHandler instead")
-    override fun registerBarcodeReceiver(eventHandler: IEventHandler) {
-        throw RuntimeException("Scanner is not available")
-    }
-
+    override fun registerBarcodeReceiver(eventHandler: IEventHandler) { }
     @Deprecated("Use registerTypedEventHandler and unregisterTypedEventHandler instead")
-    override fun unregisterBarcodeReceiver(eventHandler: IEventHandler) {
-        throw RuntimeException("Scanner is not available")
-    }
-
-    override fun registerTypedEventHandler(handler: TypedEventHandler) {
-    }
-
-    override fun unregisterTypedEventHandler(handler: TypedEventHandler) {
-    }
-
-    override fun pauseReceivers() {
-    }
-
-    override fun resumeReceivers() {
-    }
-
-    override fun setAutoEnter(value: Enabler) {
-        throw RuntimeException("Scanner is not available")
-    }
-
-    override fun setNotificationSound(value: Enabler) {
-        throw RuntimeException("Scanner is not available")
-    }
-
-    override fun setNotificationVibration(value: Enabler) {
-        throw RuntimeException("Scanner is not available")
-    }
-
-    override fun setScanMode(value: ScanMode) {
-        throw RuntimeException("Scanner is not available")
-    }
-
-    override fun setGs1Config(config: Gs1Config) {
-    }
-
+    override fun unregisterBarcodeReceiver(eventHandler: IEventHandler) { }
+    override fun registerTypedEventHandler(handler: TypedEventHandler) { }
+    override fun unregisterTypedEventHandler(handler: TypedEventHandler) { }
+    override fun pauseReceivers() { }
+    override fun resumeReceivers() { }
+    override fun setAutoEnter(value: Enabler) { }
+    override fun setNotificationSound(value: Enabler) { }
+    override fun setNotificationVibration(value: Enabler) { }
+    override fun setScanMode(value: ScanMode) { }
+    override fun setGs1Config(config: Gs1Config) { }
     override fun nfcAvailable(): Boolean {
         return false
     }
-
     override fun getNfcStatus(): Enabler {
-        throw RuntimeException("NFC is not available")
+        return Enabler.OFF
     }
-
     override fun canSetNfcStatus(): Boolean {
         return false
     }
-
-    override fun setNfcStatus(status: Enabler) {
-        throw RuntimeException("NFC is not available")
-    }
-
+    override fun setNfcStatus(status: Enabler) { }
     @Deprecated("Use registerTypedEventHandler and unregisterTypedEventHandler instead")
-    override fun registerNFCReceiver(eventHandler: IEventHandler) {
-        throw RuntimeException("NFC is not available")
-    }
-
+    override fun registerNFCReceiver(eventHandler: IEventHandler) { }
     override fun canSetSymbology(): Boolean {
         return false
     }
-
-    override fun setSymbology(symbology: Symbology) {
-        throw RuntimeException("Symbology is not available")
-    }
+    override fun setSymbology(symbology: Symbology) { }
 }
